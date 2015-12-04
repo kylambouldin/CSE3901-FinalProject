35eec3a66de4c65b94c91886ab6430d4db4ee267
kylambouldin <kylambouldin@gmail.com>
2015-12-03 20:02:02 -0500

    fixed link errors

--- fitness_challenge/config/routes.rb
+++ fitness_challenge/config/routes.rb
@@ -5,13 +5,13 @@ Rails.application.routes.draw do
 
   resources :entries
   resources :entries
-  get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  get 'home' => 'home#index'
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
